f02f5c6aff14b41cd1866dcb211b5903770fc9ab
DanGustavo1234 <dacordovaja@uide.edu.ec>
2025-12-23 21:54:35 -0500

    message bubble

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../../widgets/chat/my_message_bubble.dart';
+
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors=Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage("https://tvazteca.brightspotcdn.com/dims4/default/f47f192/2147483647/strip/true/crop/540x540+0+135/resize/720x720!/format/webp/quality/90/?url=http%3A%2F%2Ftv-azteca-brightspot.s3.amazonaws.com%2Fad%2Ff8%2F172f4c72667c0588158a6a1cd6d7%2Fa-2154490.jpg"),
+          ),
+        ),
+        title: Text("My love ",style:TextStyle(color: Colors.white),),
+        centerTitle: false,
+        backgroundColor: colors.secondary,
+      ),
+      body: ChatView(),
+    );
+  }
+}
+
+class ChatView extends StatelessWidget {
+  const ChatView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child:
+     Padding(
+       padding: const EdgeInsets.symmetric(horizontal: 10),
+       child: Column(children: [
+        Expanded(child:
+        ListView.builder(
+          itemCount: 100,
+          itemBuilder:(context, index) {
+          return MyMessageBubble();
+        },)
+        ),
+        Text("Mundo")
+           ],),
+     )); 
+   
+  }
+}
+
+
